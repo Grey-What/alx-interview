9fbd7ee8af5791992cf25c89c1924a4faf972aec
Grey <greywhat2002@gmail.com>
2024-08-19 10:51:59 +0200

    fixed typo and removed reduntent check

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,7 +4,7 @@
 import sys
 
 status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                 '403': 0, '404': 0, '405': 0, '500': 0}
+                '403': 0, '404': 0, '405': 0, '500': 0}
 
 
 total_size = 0
@@ -18,14 +18,14 @@ try:
             status_code = lines_list[-2]
             file_size = int(lines_list[-1])
 
-            if status_code in status_codes.keys():
+            if status_code in status_codes:
                 status_codes[status_code] += 1
 
             total_size += file_size
             count += 1
 
         if count == 10:
-            count == 0
+            count = 0
             print("File size: {}".format(total_size))
 
             for k, v in sorted(status_codes.items()):
