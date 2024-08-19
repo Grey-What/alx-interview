3ed4219aace4992587aab4f8b7c0baaef0b58d07
Grey <greywhat2002@gmail.com>
2024-08-19 10:35:12 +0200

    parsing http request log file

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""parses a HTTP request log file"""
+
+import sys
+
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                 '403': 0, '404': 0, '405': 0, '500': 0}
+
+
+total_size = 0
+count = 0
+
+try:
+    for line in sys.stdin:
+        lines_list = line.split(" ")
+
+        if len(lines_list) > 4:
+            status_code = lines_list[-2]
+            size = int(lines_list[-1])
+
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+
+            total_size += size
+            count += 1
+
+        if count == 10:
+            count == 0
+            print("File size: {}".format(total_size))
+
+            for k, v in sorted(status_codes.items()):
+                if v != 0:
+                    print("{}: {}".format(k, v))
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    print("File size: {}".format(total_size))
+    for k, v in sorted(status_codes.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
