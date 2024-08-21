7f47268da9fe9ae2bdc0861d76d7837d41829482
Grey <greywhat2002@gmail.com>
2024-08-21 10:36:35 +0200

    utf-8 validate data

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""utf-8 validation"""
+
+
+def validUTF8(data):
+    """
+    determines if a given data set
+    represents a valid UTF-8 encoding
+
+    Returns: True if data is a valid UTF-8 encoding, else False
+
+    Args: data (list): a list of integers
+    """
+    continue_bytes = 0
+
+    # bit pattern
+    UTF8_BIT_1 = 1 << 7
+    UTF8_BIT_2 = 1 << 6
+
+    for byte in data:
+        one_mask = 1 << 7
+
+        if continue_bytes == 0:
+
+            while one_mask & byte:
+                continue_bytes += 1
+                one_mask = one_mask >> 1
+
+            if continue_bytes == 0:
+                continue
+
+            if continue_bytes == 1 or continue_bytes > 4:
+                return False
+        else:
+            if not (byte & UTF8_BIT_1 and not (byte & UTF8_BIT_2)):
+                return False
+
+            continue_bytes -= 1
+
+    if continue_bytes != 0:
+        return False
+    return True
